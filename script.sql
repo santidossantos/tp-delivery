24822e8fdf0c2db7513c2c8631878ecfb4c7194d
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-04-21 09:19:43 -0300

    Permisos Script SQ

--- script.sql
+++ script.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS bd2_2023_grupo6;
 CREATE USER IF NOT EXISTS 'usuario'@'localhost' IDENTIFIED BY 'password';
 ALTER USER 'usuario'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT CREATE, DROP, ALTER, SELECT, INSERT, UPDATE ON bd2_2023_grupo6.* TO 'usuario'@'localhost';
+GRANT CREATE, ALTER, SELECT, INSERT, UPDATE ON bd2_2023_grupo6.* TO 'usuario'@'localhost';
 FLUSH PRIVILEGES;
